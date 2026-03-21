a1754b40d3db41e5c7e58c47a0f4234ef6a9b170
Esther <elauder549@gmail.com>
2026-03-21 01:18:20 +0100

    Esther merges forked out branch back to main

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function loading() {
-  return <div>loading</div>;
+  //Added styles: Esther.
+  return (
+    <div
+      style={{
+        color: "white",
+        textAlign: "center",
+        fontSize: "2rem",
+      }}
+    >
+      loading...
+    </div>
+  );
 }
