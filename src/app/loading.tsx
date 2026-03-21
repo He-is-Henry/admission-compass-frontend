42a516dcbbab6a2df0651b7b50920c046f74fa24
Esther <elauder549@gmail.com>
2026-03-21 01:20:40 +0100

    final merge

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,5 @@
 import React from "react";
 
 export default function loading() {
-  //Added styles: Esther.
-  return (
-    <div
-      style={{
-        color: "white",
-        textAlign: "center",
-        fontSize: "2rem",
-      }}
-    >
-      loading...
-    </div>
-  );
+  return <div>loading</div>;
 }
