971aea60810363516c1367ea3a3b02570dd6f618
Henry <Henries90@gmail.com>
2026-04-13 22:40:27 +0100

    Added sitemaps and robots

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,6 +12,10 @@ export default async function sitemap() {
 
   return [
     {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
       url: `${baseUrl}/about`,
       lastModified: new Date(),
     },
